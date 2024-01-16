1ec8e8310a87cb58d6a62d27ffb7f286ce29cc24
farzad83f83h83 <156295786+farzad83f83h83@users.noreply.github.com>
2024-01-16 15:45:10 +0330

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,20 @@ const nav = document.querySelector(".mobile-nav"),
  const navTogglefunc= () => nav.classList.toggle("active");
  navMenuBtn.addEventListener("click", navTogglefunc);
  navCloseBtn.addEventListener("click", navTogglefunc);
+
+
+ //theme toggle variables
+ const themeBtn = document.querySelectorAll('.theme-btn');
+
+ for(let i = 0; i < themeBtn.length; i++) {
+    themeBtn[i].addEventListener("click",function(){
+        document.body.classList.toggle(".light-theme");
+        document.body.classList.toggle("dark-theme");
+    
+
+        for(let j =0; j < themeBtn.length; j++) {
+            themeBtn[j].classList.toggle('light');
+            themeBtn[j].classList.toggle('dark');
+        }
+    })
+ }
